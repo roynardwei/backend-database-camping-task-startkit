087ee63f2da519b93ed7ce466e5bd990e9ffcc61
Roynardwei <yuishang@gmail.com>
2024-12-02 09:40:31 +0800

    update 1-1

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -13,7 +13,16 @@
 --     3. 用戶名稱為`肌肉棒子`，Email 為`muscle@hexschooltest.io`，Role為`USER`
 --     4. 用戶名稱為`好野人`，Email 為`richman@hexschooltest.io`，Role為`USER`
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
---     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
+--     6. 用戶名稱為 `透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
+        insert into user (name, email, role)
+        values 
+        ('李燕容', 'lee2000@hexschooltest.io', 'USER'),
+        ('王小明', 'wXlTq@hexschooltest.io', 'USER'),
+        ('肌肉棒子', 'muscle@hexschooltest.io', 'USER'),
+        ('好野人', 'richman@hexschooltest.io'),
+        ('Q太郎', 'starplatinum@hexschooltest.io'),
+        ('透明人', 'opacity0@hexschooltest.io', 'USER'),
+
 
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 
