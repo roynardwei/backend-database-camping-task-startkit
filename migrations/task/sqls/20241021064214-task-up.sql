60fa0fe9d79a5a247dc4ed261616dc56389f9256
Roynardwei <yuishang@gmail.com>
2024-12-04 14:17:36 +0800

    update Task 3-3 done

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -140,4 +140,25 @@
     (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
     (SELECT id FROM "SKILL" WHERE name = '復健訓練')
     );
+-- 3-3 修改：更新教練的經驗年數，資料需求如下：
+    -- 1. 教練`肌肉棒子` 的經驗年數為3年
+    -- 2. 教練`Q太郎` 的經驗年數為5年
+    --主要使用 update 更新COACH表 where 找出user_id再從USER表使用email找id.
+    3-3-1
+    update "COACH"
+    set experience_years = 3
+    where user_id =(
+    SELECT id 
+    from "USER" 
+    where "email" = 'muscle@hexschooltest.io'
+    );
+
+    3-3-2
+    update "COACH"
+    set experience_years = 5
+    where user_id =(
+    SELECT id
+    from "USER"
+    WHERE "email" = 'starplatinum@hexschooltest.io'
+    );
 
