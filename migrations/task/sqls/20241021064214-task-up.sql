d6a13492d1d04d5c2698a7ac8c0f54f26a09b763
Roynardwei <yuishang@gmail.com>
2024-12-04 11:29:33 +0800

    update 1-4 LV1

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -44,7 +44,7 @@
     SELECT COUNT(*) --取得用戶數量
     FROM "USER";
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
-    SELECT * FROM USER LIMIT 3; --選取資料表中的三筆資料
+    SELECT * FROM "USER" LIMIT 3; --選取資料表中的三筆資料
 
 --  ████████  █████   █    ████  
 --    █ █   ██    █  █         █ 
