e19d24a887169babfbd8686d453bc45255494141
Roynardwei <yuishang@gmail.com>
2024-12-04 13:58:51 +0800

    update TASK2 done

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -67,22 +67,22 @@
     -- 3. `好野人` 購買 `14 堂組合包方案`
     INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) VALUES
     (
-        (SELECT id FROM "USER" WHERE "email" = 'wXlTq@hexschooltest.io'),
-        (SELECT id FROM "CREDIT_PURCHASE" WHERE name ='14 堂組合包方案'),
-        (SELECT credit_amount FROM "CREDIT_PURCHASE" WHERE name ='14 堂組合包方案'),
-        (SELECT price FROM "CREDIT_PURCHASE" WHERE name ='14 堂組合包方案')
+        (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
+        (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14堂組合包方案'),
+        (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14堂組合包方案'),
+        (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14堂組合包方案')
     ),
     (
-        (SELECT id FROM "USER" WHERE "email" = 'wXlTq@hexschooltest.io'),
-        (SELECT id FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案'),
-        (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案'),
-        (SELECT price FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案')
+        (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
+        (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '21堂組合包方案'),
+        (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '21堂組合包方案'),
+        (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '21堂組合包方案')
     ),
     (
-        (SELECT id FROM "USER" WHERE "email" = 'richman@hexschooltest.io'),
-        (SELECT id FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
-        (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
-        (SELECT price FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案')
+        (SELECT id FROM "USER" WHERE email = 'richman@hexschooltest.io'),
+        (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14堂組合包方案'),
+        (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14堂組合包方案'),
+        (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14堂組合包方案')
     );  
 
 
