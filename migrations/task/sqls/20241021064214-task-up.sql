3ad0ce03c06613e8d34c13627e691e4af7f45dde
Roynardwei <yuishang@gmail.com>
2024-12-04 13:50:28 +0800

    update TASK2 done

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -68,22 +68,22 @@
     INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid)
     VALUES
     (
-        (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
-        (SELECT id FROM "CREDIT_PURCHASE" WHERE name ='14 堂組合包方案'),
-        (SELECT credit_amount FROM "CREDIT_PURCHASE" WHERE name ='14 堂組合包方案'),
-        (SELECT price FROM "CREDIT_PURCHASE" WHERE name ='14 堂組合包方案')
+        (SELECT id FROM "USER" WHERE "email" = 'wXlTq@hexschooltest.io'),
+        (SELECT id FROM "CREDIT_PURCHASE" WHERE "name" ='14 堂組合包方案'),
+        (SELECT credit_amount FROM "CREDIT_PURCHASE" WHERE "name" ='14 堂組合包方案'),
+        (SELECT price FROM "CREDIT_PURCHASE" WHERE "name" ='14 堂組合包方案')
     ),
     (
-        (SELECT id FROM "USER" WHERE name = '王小明'),
-        (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
-        (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
-        (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案')
+        (SELECT id FROM "USER" WHERE "name" = '王小明'),
+        (SELECT id FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案'),
+        (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案'),
+        (SELECT price FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案')
     ),
     (
-        (SELECT id FROM "USER" WHERE name = '好野人'),
-        (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
-        (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
-        (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案')
+        (SELECT id FROM "USER" WHERE "name" = '好野人'),
+        (SELECT id FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
+        (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
+        (SELECT price FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案')
     );  
 
 
